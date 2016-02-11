834adc386f85b3ec45eeb6747b35145ed0f4fcca
marc <marc@refamco.ca>
2016-02-10 21:59:20 -0500

    remove  "load url from future" as its the future! :) django 1.9.2 support

--- django_admin_bootstrapped/templatetags/bootstrapped_goodies_tags.py
+++ django_admin_bootstrapped/templatetags/bootstrapped_goodies_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.loader import render_to_string, TemplateDoesNotExist
-from django.utils.importlib import import_module
+from importlib import import_module
+#from django.utils.importlib import import_module
 
 register = template.Library()
 
